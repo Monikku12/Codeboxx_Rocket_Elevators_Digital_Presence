9fc7b0a63e38c9b2ceb9c8dccdf3ed73c0fce7b4
Monikku12 <monique.paymentboulanger@gmail.com>
2022-05-31 04:06:22 +0900

    JS Building tyoe = Done, JS Results = in construction

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,19 +1,50 @@
 /**	QUOTE FORM
 *************************************************** **/
 
-$(document).ready(function () {
-    $('input[type="radio"]').click(function () {
-        if ($(this).attr("value") == "red") {
-            $(".box").hide();
-            $(".red").show();
-        }
-        if ($(this).attr("value") == "green") {
-            $(".box").hide();
-            $(".green").show();
-        }
-        if ($(this).attr("value") == "blue") {
-            $(".box").hide();
-            $(".blue").show();
-        }
-    });
-});
+$(function() {
+    $("input[name='building-type-radio']").click(function() {
+      if ($("#residential").is(":checked")) {
+        $("#number-of-apartments").show();
+        $("#number-of-floors").show();
+        $("#number-of-basements").show();
+        $("#number-of-companies").hide();
+        $("#number-of-parking-spots").hide();
+        $("#number-of-elevators").hide();
+        $("#number-of-corporations").hide();
+        $("#maximum-occupancy").hide();
+        $("#business-hours").hide();
+    } if ($("#commercial").is(":checked")) {
+        $("#number-of-companies").show();
+        $("#number-of-floors").show();
+        $("#number-of-basements").show();
+        $("#number-of-parking-spots").show();
+        $("#number-of-elevators").show();
+        $("#number-of-apartments").hide();
+        $("#number-of-corporations").hide();
+        $("#maximum-occupancy").hide();
+        $("#business-hours").hide();
+    } if ($("#corporate").is(":checked")) {
+        $("#number-of-corporations").show();
+        $("#number-of-floors").show();
+        $("#number-of-basements").show();
+        $("#number-of-parking-spots").show();
+        $("#maximum-occupancy").show();
+        $("#number-of-apartments").hide();
+        $("#number-of-companies").hide();
+        $("#number-of-elevators").hide();
+        $("#business-hours").hide();
+    } if ($("#hybrid").is(":checked")) {
+        $("#number-of-companies").show();
+        $("#number-of-floors").show();
+        $("#number-of-basements").show();
+        $("#number-of-parking-spots").show();
+        $("#maximum-occupancy").show();
+        $("#business-hours").show();
+        $("#number-of-corporations").hide();
+        $("#number-of-elevators").hide();
+        $("#number-of-apartments").hide();        
+    }
+            });
+        })
+
+ 
