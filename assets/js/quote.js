88c5381f58d5bbebe0dcac5ec5b0950891b5c705
Monikku12 <monique.paymentboulanger@gmail.com>
2022-06-01 02:18:43 +0900

    for coach

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,6 +1,7 @@
 /**	QUOTE FORM
 *************************************************** **/
 
+// BUILDING TYPES FUNCTIONS --------------------------------
 $(function() {
     $("input[name='building-type-radio']").click(function() {
       if ($("#residential").is(":checked")) {
@@ -43,8 +44,50 @@ $(function() {
         $("#number-of-corporations").hide();
         $("#number-of-elevators").hide();
         $("#number-of-apartments").hide();        
-    }
-            });
+    }});
+        $("#number-of-apartments").change(function(){
+            var numberOfApartments = $("#number-of-apartments").val();
+            console.log("number-of-apartments is:", numberOfApartments);
+    });
         })
 
- 
+        
+
+       
+
+// // GET DATA FROM HTML FORM
+//     var numberOfApartments = document.getElementById("#number-of-apartments").value;
+//     var numberOfFloors = document.getElementById("#number-of-floors").val();
+//     var numberOfBasements = document.getElementById("#number-of-basements").val();
+//     var numberOfCompanies = document.getElementById("#number-of-companies").val();
+//     var numberOfParkingSpots = document.getElementById("#number-of-parking-spots").val();
+//     var numberOfElevators = document.getElementById("#number-of-elevators").val();
+//     var numberOfCorporations = document.getElementById("#number-of-corporations").val();
+//     var maximumOccupancy = document.getElementById("#maximum-occupancy").val();
+//     var businessHours = document.getElementById("#business-hours").val();
+
+// console.log("numberOfApartments")
+
+
+// ESTOMATE CALCULATION FUNCTIONS 
+    // Variables definitions
+    // var elevatorAmount = $("#elevator-amount").val()
+    // var elevatorUnitPrice = $("#elevator-unit-price").val()
+    // var elevatorTotalPrice = $("#elevator-total-price").val()
+    // var installationFees = $("#installation-fees").val()
+    // var finalPrice = $("#final-price").val()
+
+    // console.log("elevatorAmount is:", elevatorAmount);
+    // console.log("elevatorUnitPrice is:", elevatorUnitPrice);
+    
+
+
+
+
+// $(function() {
+//     $("input[name='building-type-radio']").click(function() {
+//       if ($("#commercial").is(":checked")) { 
+//         $"#number-of-elevators" = elevatorAmount;
+//     }
+//             });
+//         })
